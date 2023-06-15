5e133a7827adcd4d528ea75d769bcd08ba973ee9
YashwanthGowda07 <yashwanthgowdag96@gmail.com>
2023-06-15 19:55:25 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+export default NumberManagementService;
 
-function App() {
+function NumberManagementService() {
+  const [numbers, setNumbers] = useState([]);
+
+  useEffect(() => {
+    const fetchNumbers = async () => {
+      const urls = [
+        'http://104.211.219.98/numbers/primes',
+    'http://104.211.219.98/numbers/fibo',
+    'http://104.211.219.98/numbers/odd',
+    'http://104.211.219.98/numbers/rand'
+      ];
+
+      try {
+        const responseList = await Promise.all(urls.map(url => axios.get(url)));
+        const fetchedNumbers = responseList.reduce((acc, response) => {
+          if (response.status === 200) {
+            return acc.concat(response.data.numbers || []);
+          }
+          return acc;
+        }, []);
+
+        setNumbers(fetchedNumbers);
+      } catch (error) {
+        console.error('Error fetching numbers:', error);
+      }
+    };
+
+    fetchNumbers();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Numbers:</h1>
+      <ul>
+        {numbers.map((number, index) => (
+          <li key={index}>{number}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+
